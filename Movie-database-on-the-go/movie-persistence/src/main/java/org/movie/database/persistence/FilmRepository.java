ba25832c68a9b1fb2ae93ef772bcffc446e5f772
readywer <90259918+readywer@users.noreply.github.com>
2024-03-31 15:09:12 +0200

    fix registration, delet unused ropository

--- Movie-database-on-the-go/movie-persistence/src/main/java/org/movie/database/persistence/FilmRepository.java
+++
@@ -1,9 +0,0 @@
-package org.movie.database.persistence;
-
-import org.movie.database.domain.Film;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FilmRepository extends CrudRepository<Film, Long> {
-}
